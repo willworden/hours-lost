0ea58279a3b2668f0a17208713b910401795c60e
willworden <willworden@hotmail.com>
2013-08-08 10:44:21 -0500

    adding XML loader, some refactoring

---
+++ src/main/java/com/thoughtworks/plugins/hours_lost/XMLLoader.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.plugins.hours_lost;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworker
+ * Date: 8/8/13
+ * Time: 10:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XMLLoader {
+}
